124ea881b8bc1a7ed5c793fa9f7c9a8c3257bde8
Janderson Lima Costa <janderson.costa@ufpe.br>
2025-01-31 09:53:38 -0300

    FEAT: functions in the main program

---
+++ mercado.py
@@ -0,0 +1,36 @@
+from typing import List, Dict
+from time import sleep
+
+from models.produto import Produto
+from utils.helper import formata_float_str_moeda
+
+produtos = list(Produto) = []
+carrinho = list[Dict[Produto, int]] = []
+
+def main() -> None:
+    menu()
+    
+def menu() -> None:
+    pass
+
+def cadastrar_produto() -> None:
+    pass
+
+def listar_produto() -> None:
+    pass
+
+def comprar_produto() -> None:
+    pass
+
+def visualizar_carrinho() -> None:
+    pass
+
+def fechar_pedido() -> None:
+    pass
+
+def selecionar_produto_por_codigo(codigo: int) -> None:
+    pass
+
+if __name__ == '__main__':
+    main()
+
