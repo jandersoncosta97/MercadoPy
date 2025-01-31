72e69e52677f6b9a65430076ccfdee3f93a5c9ad
Janderson Lima Costa <janderson.costa@ufpe.br>
2025-01-31 09:54:44 -0300

    FIX (Produto): __init__  -> typing error

--- models/produto.py
+++ models/produto.py
@@ -3,7 +3,7 @@ from utils.helper import formata_float_str_moeda
 class Produto:
     contador = 1
     
-    def __init__(self, object, nome: str, preco: float) -> None:
+    def __init__(self: object, nome: str, preco: float) -> None:
         self.__codigo: int = Produto.contador
         self.__nome: str = nome
         self.__preco: float = preco
